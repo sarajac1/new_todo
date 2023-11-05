89dc51edf60c222fabcce8bfd378b82df1fbef41
sarajac1 <sarajac@hotmail.com>
2023-11-05 12:34:48 +0100

    Modified HTML document to fix connection to js Made todoList into an object lieral

--- todo.js
+++ todo.js
@@ -1,38 +1,43 @@
 //1. Deklarera en variabel med namnet todoList
-let todoList = ['Boka tandläkartid', 'Uppdatera CVt'];
+let todoList = {
+  items: [],
+  
+  addToList(description) {
+    let item = { description};
+    this.items.push(item)
+    console.log(this.items)
+  }
 
-
-//2. Skapa en funktion addToList som lägger till en sak att göra på listan och returnerar hela listan
-function addToList(todoItem) {
-  todoList.push(todoItem)
-  return todoList;
 }
 
 
+
+/* 
+/* 
 /*3. Lägg till en sak att göra överst i listan
 Ibland får man saker att göra som är viktigare än alla andra.Skapa en funktion addToTopOfList som lägger till en sak att göra längst upp i listan, samt returnerar hela listan.*/
 
-
+/* 
 function addToTopOfList(todoItem) {
   todoList.unshift(todoItem)
   return todoList;
-}
+} */
 
 /*4. Ta bort en sak längst ner i listan
 Kanske var det där längst ner i att göra listan inte så viktigt?Skriv en funktion removeFromBottomOfList som tar bort den sak som är längst ner i listan och returnerar den.*/
-
+/* 
 function removeFromBottomOfList(todoItem) {
   todoList.pop(todoItem)
   return todoList;
-}
+} */
 
 /* 5. Ta bort en sak högst upp i listan
 Du fick äntligen det som var viktigast gjort?Skriv en funktion removeFromTopOfList som tar bort den sak som är högst i listan och returnerar den */
-
+/* 
 function removeFromTopOfList(todoItem) {
   todoList.shift(todoItem)
   return todoList;
-}
+} */
 
 /*6. Ta bort en sak baserad på dess placering i listan
 Det är inte alltid man får saker gjorda i ordning.Skriv en funktion removeFromListByIndex till vilken man skickar ett nummer och saken på denna plats i listan tas bort.Returnera den bortplockade saken.
@@ -42,7 +47,7 @@ Det är inte alltid man får saker gjorda i ordning.Skriv en funktion removeFrom
     Notera 2: Tillåt INTE att man försöker ta bort saker på negativa platser, dvs.skickar ett nummer mindre än 0.*/
 
 
-function removeFromListByIndex(index) {
+/* function removeFromListByIndex(index) {
   if (index < 0) {
     return null;
   }
@@ -50,20 +55,20 @@ function removeFromListByIndex(index) {
     return null;
   }
   return todoList.splice(index, 1)[0];
-}
+} */
 
 /* 7. Ta bort en sak baserad på dess namn
 Skriv en funktion removeFromListByName som hittar en sak i listan baserat på dess namn och tart bort den.Returnera den borttagna saken.
 
   Notera: Försök att skriva mindre kod genom att återanvända / anropa removeFromListByIndex(från steg 6) inuti din funktion. */
 
-function removeFromListByName(todoItem) {
+/* function removeFromListByName(todoItem) {
   for (let index = 0; index <= todoList.length; index++) {
     if (todoList[index] === todoItem) {
       return todoList.splice(index, 1)[0];
     }
   }
-}
+} */
 
 /* 8. Ta bort en sak och lägg till den i ”har gjort”-lista
 Skapa en att ”har gjort”-lista genom att deklarera en variabel med namnet doneList som en array.
@@ -71,20 +76,20 @@ Skriv en funktion removeFromListAndAddToDone som flyttar en sak from att göra -
 
   Notera: Försök att skriva mindre kod genom att återanvända / anropa removeFromListByName(från steg 7) inuti din funktion. */
 
-let doneList = [];
+/* let doneList = [];
 
 function removeFromListAndAddToDone(todoItem) {
   let index = todoList.indexOf(todoItem)
   doneList = todoList.splice(index, 1)[0];
 
 
-}
+} */
 
 /* 9. Flytta en sak till toppen av listan
 Ibland blir en sak plötsligt riktigt viktig att göra.Skriv en funktion moveToTop som hittar en sak i listan baserat på dess namn och flyttar den till toppen.Returnera hela att göra - listan.
 
   Notera: Försök att skriva mindre kod genom att återanvända / anropa removeFromListByName(från steg 7) och addToTopOfList(från steg 3) inuti din funktion. */
-
+/* 
 function moveToTop(todoList, todoItem) {
   let index = todoList.indexOf(todoItem)
 
@@ -94,7 +99,7 @@ function moveToTop(todoList, todoItem) {
   }
   return todoList;
 
-}
+} */
 
 
 
@@ -103,14 +108,14 @@ Ibland blir en sak plötsligt ganska oprioriterad.Skriv en funktion moveToBottom
 
   Notera: Försök att skriva mindre kod genom att återanvända / anropa removeFromListByName(från steg 7) och addToList(från steg 2) inuti din funktion. */
 
-function moveToBottom(todoList, todoItem) {
+/* function moveToBottom(todoList, todoItem) {
   let index = todoList.indexOf(todoItem)
   if (index !== -1) {
     let removedItem = todoList.splice(index, 1)[0]
     todoList.push(removedItem)
   }
   return todoList;
-}
+} */
 
 
 /* 11. Flytta en sak ett steg ner i listan
@@ -118,7 +123,7 @@ Ibland behöver man prioritera ner något lite grann.Skriv en funktion moveDown 
 
   Tips: Man kan även se detta som att saken byter plats med det som var nedanför den. */
 
-function moveDown(todoList, todoItem) {
+/* function moveDown(todoList, todoItem) {
   let index = todoList.indexOf(todoItem);
   if (index !== -1 && index < todoList.length - 1) {
     let temp = todoList[index];
@@ -126,14 +131,14 @@ function moveDown(todoList, todoItem) {
     todoList[index + 1] = temp;
   }
   return todoList;
-}
+} */
 
 /* 12. Flytta en sak ett steg upp i listan
 Ibland behöver man prioritera upp något lite grann.Skriv en funktion moveUp som flyttar en sak ett steg upp i listan.
 
   Tips: Man kan även se detta som att saken byter plats med det som var ovanför den. */
 
-function moveUp(todoList, todoItem) {
+/* function moveUp(todoList, todoItem) {
   let index = todoList.indexOf(todoItem);
   if (index > 0) {
     let temp = todoList[index];
@@ -141,4 +146,4 @@ function moveUp(todoList, todoItem) {
     todoList[index - 1] = temp;
   }
   return todoList;
-}
+} */
