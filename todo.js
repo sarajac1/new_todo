ddf796d4eadb24b501ada1efdd150c1cd42f1869
sarajac1 <sarajac@hotmail.com>
2024-01-18 19:06:37 +0100

    Added stringify to display items that are modified. Added new function to display to do list.

--- todo.js
+++ todo.js
@@ -25,14 +25,22 @@ class todoListItem {
   
   //Funktion som lägger till en ny sak i listan
   addToList() {
-    todoList.items.push({
+  let newItem = {
       title: this.title,
       description: this.description,
       priority: this.priority,
       deadline: this.deadline
-    });
-    console.log(`Successfully added item to the list: \nTitle: ${this.title}\nDescription: ${this.description}\nPriority: ${this.priority}\nDeadline: ${this.deadline}`);
+    };
+    todoList.items.push(newItem);
+    console.log(`Successfully added item to the list:\n${JSON.stringify(newItem, null, 2)}`);
+
   }
+
+  //Funktion som visar listans innehåll
+  displayList() {
+    console.log(`To do list:\n${JSON.stringify(todoList.items, null, 2)}`);
+  }
+
 
   //Funktion som sparar listans innehåll till fill 
   saveToFile(fileName) {
@@ -64,47 +72,43 @@ class todoListItem {
       deadline: this.deadline
     };
     todoList.items.unshift(newItem);
-    console.log(`Successfully added to the top of the list:\nTitle: ${this.title}\nDescription: ${this.description}\nPriority: ${this.priority}\nDeadline: ${this.deadline}`);
+    console.log(`Successfully added to the top of the list:\n${JSON.stringify(newItem, null, 2)}`)
     }
-
-
 
 
   //Funktion för att ta bort en sak längst ner i listan
   removeFromBottomOfList() {
     let removedItem = todoList.items.pop();
     console.log(`Successfully removed the following item from the bottom of the list:\n${JSON.stringify(removedItem, null, 2)}`);
-
-    
   }
 
   //Funktion för att ta bort en sak högst upp i listan/först saken i en array
   removeFromTopofList() {
-    todoList.items.shift();
-    console.log("Successfully removed from the top of the list.")
+    let removedItem = todoList.items.shift();
+    console.log(`Successfully removed from the top of the list:\n${JSON.stringify(removedItem, null, 2)}`)
   }
   
-
 
   //Ta bort en sak baserad på dess placering i listan
   removeFromListByIndex(index) {
     if (index < 0 || index >= todoList.items.length) {
       return null;
     }
-    todoList.items.splice(index, 1)[0];
-    console.log("Successfully removed.")
+    let removedItem = todoList.items.splice(index, 1)[0];
+    console.log(`Successfully removed item with index ${index}:\n${JSON.stringify(removedItem, null, 2)} `)
   }
+  
 
   //Ta bort en sak baserad på dess namn
-
   removeFromListByName(title) {
+    let removedItem = null;
     for (let index = 0; index < todoList.items.length; index++) {
       if (todoList.items[index].title === title) {
-        todoList.items.splice(index, 1);
+        removedItem = todoList.items.splice(index, 1);
       }
       
     }
-    console.log("Successfully removed.")
+    console.log(`Successfully removed item:\n${JSON.stringify(removedItem, null, 2)}`)
   }
 
   //Ta bort en sak och lägg till den i en "har gjort"-lista (deklarerad som en array ovan), 
@@ -112,9 +116,8 @@ class todoListItem {
     if (index >= 0 && index < todoList.items.length) {
       let doneItem = todoList.items.splice(index, 1)[0]
       doneList.items.push(doneItem)
+      console.log(`Successfully added to done list:\n${JSON.stringify(doneItem, null, 2)}`)
     }
-    console.log("Successfully added to done list.")
-
   }
 
   //Flytta en sak till toppen av listan
@@ -123,8 +126,8 @@ class todoListItem {
     if (index !== -1) {
       let movedItem = todoList.items.splice(index, 1)[0];
       todoList.items.unshift(movedItem); 
+      console.log(`Successfully moved to the top:\n${JSON.stringify(movedItem, null, 2)}`)
     }
-    console.log("Successfully moved to the top.")
   }
 
   //Flytta en sak till botten av listan
@@ -133,29 +136,30 @@ class todoListItem {
     if (index !== -1) {
       let movedItem = todoList.items.splice(index, 1)[0];
       todoList.items.push(movedItem);
-      
+      console.log(`Successfully moved to the bottom:\n${JSON.stringify(movedItem, null, 2)}`)
     }
-    console.log("Successfully moved item to the bottom.")
-
   }
 
   //Flytta en sak ett steg ner i listan
   moveDown(title) {
     let index = todoList.items.findIndex(item => item.title === title);
     if (index !== -1 && index < todoList.items.length) {
+      let movedItem = todoList.items[index];
       [todoList.items[index], todoList.items[index + 1]] = [todoList.items[index + 1], todoList.items[index]]
+      console.log(`Successfully moved item down a step:\n${JSON.stringify(movedItem, null, 2)}`);
+
     }
-    console.log("Successfully moved item down a step.")
+
   }
 
   //Flytta en sak ett steg upp i listan
   moveUp(title) {
     let index = todoList.items.findIndex(item => item.title === title);
     if (index > 0) {
+      let movedItem = todoList.items[index];
       [todoList.items[index], todoList.items[index - 1]] = [todoList.items[index - 1], todoList.items[index]]
+    console.log(`Successfully moved item up a step:\n${JSON.stringify(movedItem, null, 2)} `)
     }
-    console.log("Successfully moved item up a step.")
+
   }
-
-
 }
